9fca172152c37986db27926bd602e00907cd91cb
dimkon97 <dkonstan@mit.edu>
2019-05-07 18:00:43 -0400

    Update random_forrest.R

--- random_forrest.R
+++ random_forrest.R
@@ -1,5 +1,5 @@
 library(caret)
-#use dataset called "data" for training and testing models
+#use dataset calles data for training and testing models
 #split dataset
 set.seed(156)
 split <- createDataPartition(data$avg_song_popularity, p = 0.8, list = FALSE)
@@ -19,7 +19,7 @@ data.test <- data[-split,]
 
 #run rf model with cross validation
 rf.cv <- train(y =  data.train$avg_song_popularity,
-               x = subset(data.train, select=-c(avg_song_popularity, song_name, album_names)),
+               x = subset(data.train, select=-c(avg_song_popularity, song_name, album_names, artist)),
                method="rf", ntree=100, nodesize = 20,
                trControl=trainControl(method="cv", number=5),
                tuneGrid=data.frame(mtry=seq(1,8,1)),
@@ -42,15 +42,17 @@ importance(rf.cv.final, scale = T)
 rf.cv.final$importance
 
 #make scaled barplot 
-imp_inc_mse <- importance(rf.cv.final, scale = T)[,1]
-imp_node_purity <- rf.cv.final$importance[,2]*100/sum(rf.cv.final$importance[,2])
+imp_inc_mse <- sort(importance(rf.cv.final, scale = T)[,1]/max(importance(rf.cv.final, scale = T)[,1])*100, decreasing =F)
+imp_node_purity <- sort(rf.cv.final$importance[,2]*100/sum(rf.cv.final$importance[,2]), decreasing = F)
 #barplot of incMSE
 par(mai=c(1,2,1,1))
-bar1<- barplot(imp_inc_mse, horiz = T, las = 1, col = "red", main = "%IncMSE", xlim = c(0,100))
+bar1<- barplot(imp_inc_mse, horiz = T, las = 1, col = "purple", main = "%IncMSE", xlim = c(0,110),
+               border = F)
 text(y=bar1, x=imp_inc_mse, pos=4,labels=round(imp_inc_mse, digits = 2))
 #barplot of node purity
 par(mai=c(1,2,1,1))
-bar2 <- barplot(imp_node_purity, horiz = T, las = 1, col = "red", main = "Inc Node Purity", xlim = c(0,100))
+bar2 <- barplot(imp_node_purity, horiz = T, las = 1, col = "purple", main = "Inc Node Purity", xlim = c(0,80),
+                border = F)
 text(y=bar2, x=imp_node_purity, pos=4,labels=round(imp_node_purity, digits = 2))
 
 # Calculate R2, OSR2, out of sample RMSE
@@ -72,7 +74,5 @@ RMSE
 plot(data.test$avg_song_popularity, col = "red", main = "Actual vs Fitted (RF)" )
 lines(pred.test, type = "p", col = "blue")
 legend(legend = c("Actual","Fitted"), fill = c("red","blue"), "topright", cex = 0.5)
-
-
 
 
