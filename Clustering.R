8391b58e5fe6aca395a60ceb411a113863eacc43
abishalom <abishalom1997@gmail.com>
2019-05-08 17:16:15 -0400

    Updated clustering and LASSO

--- Clustering.R
+++ Clustering.R
@@ -5,18 +5,30 @@ to_clust = subset(songs_data, select = -c(X, song_name, avg_song_popularity))
 d <- dist(to_clust)
 hclust.mod <- hclust(d, method="ward.D2")
 plot(hclust.mod, labels=F, ylab="Dissimilarity")
+
+
 hc.dissim <- data.frame(k = seq_along(hclust.mod$height),
                         dissimilarity = rev(hclust.mod$height))
+
 plot(hc.dissim$k, hc.dissim$dissimilarity, type="l")
 plot(hc.dissim$k, hc.dissim$dissimilarity, type="l", xlim=c(0,25))
 
-h.clusters <- cutree(hclust.mod, 11)
-table(songs_data$avg_song_popularity, h.clusters)
+h.clusters <- cutree(hclust.mod, 5)
+
+rect.hclust(hclust.mod , k = 5, border = 2:6)
+abline(h = 5, col = 'red')
+
+
+table(cut(songs_data$avg_song_popularity, 10), h.clusters)
 table(h.clusters)
 
 set.seed(15071)
-km <- kmeans(to_clust, iter.max=100, 13)
+km <- kmeans(to_clust, iter.max=100, 5)
 kmc <- km$cluster
-table(songs_data$avg_song_popularity, kmc)
+table(cut(songs_data$avg_song_popularity, 10), kmc)
 table(kmc)
 table(h.clusters, kmc)
+
+aggregate(subset(songs_data, select = -c(X, song_name)), by = list(h.clusters), mean)
+
+aggregate(subset(songs_data, select = -c(X, song_name)), by = list(kmc), mean)
